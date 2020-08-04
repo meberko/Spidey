9c08f0e446c8d7207fb480dc4ad3dc47437c8de3
meberko <meb2235@columbia.edu>
2020-08-04 19:21:47 -0400

    - Redid "About" section... Looks like this might end up being the only section. - Need to flesh out About > Projects and About > Skills

--- static/main.js
+++ static/main.js
@@ -1,6 +1,7 @@
 var navbar_height = document.getElementById("navbar").offsetHeight;
 var about_location = document.getElementById("about").offsetTop-navbar_height
 var projects_location = document.getElementById("projects").offsetTop-navbar_height
+var margin = 30
 
 locations = {
     home: navbar_height,
@@ -10,19 +11,12 @@ locations = {
 
 function load() {
     window.scrollTo({top: navbar_height, behavior: 'smooth'});
-    var about_location = document.getElementById("about").offsetTop-navbar_height
-    var projects_location = document.getElementById("projects").offsetTop-navbar_height
-    locations = {
-        home: navbar_height,
-        about: about_location,
-        projects: projects_location
-    };
-    console.log(document.getElementById("about"));
+    resize()
 };
 
 function resize() {
-    var about_location = document.getElementById("about").offsetTop-navbar_height
-    var projects_location = document.getElementById("projects").offsetTop-navbar_height
+    var about_location = document.getElementById("about").offsetTop-navbar_height-margin
+    var projects_location = document.getElementById("projects").offsetTop-navbar_height-margin
     locations = {
         home: navbar_height,
         about: about_location,
@@ -35,8 +29,6 @@ function scroll() {
     if(curr >= locations["home"] && curr < 0.95*locations["about"]) toggle_active("navbar-home");
     else if(curr >= 0.95*locations["about"] && curr < 0.95*locations["projects"]) toggle_active("navbar-about");
     else if(curr >= 0.95*locations["projects"]) toggle_active("navbar-projects");
-    console.log(document.getElementById("about").offsetTop);
-    console.log(curr);
 };
 
 function toggle_active(id) {
