dfbf4338d9eee53ec3e03292c7357a37ab33708a
meberko <meb2235@columbia.edu>
2020-08-04 15:17:18 -0400

    -Adding LICENSE and .gitignore files

--- run.py
+++ run.py
@@ -5,8 +5,4 @@ app=Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/projects")
-def projects():
-    return render_template("projects.html")
-
 if __name__=="__main__": app.run(debug=True)
